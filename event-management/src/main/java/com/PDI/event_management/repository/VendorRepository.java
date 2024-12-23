33d6c07f13cd37b3679d9f61918807639e3ce7d9
Nishitgupta0104 <nishitgupta0104@outlook.com>
2024-12-23 11:47:53 +0530

    Delete event-management directory

--- event-management/src/main/java/com/PDI/event_management/repository/VendorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PDI.event_management.repository;
-
-import com.PDI.event_management.model.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-}
-
--- event-management/src/main/java/com/PDI/event_management/repository/VenueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PDI.event_management.repository;
-
-import com.PDI.event_management.model.Venue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-}
-
